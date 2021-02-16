50f42cd4c420b8dbe08845e2f2e26181e80ff5b8
Diego Garcia Cordeiro Souza <diego00alfa@gmail.com>
2021-02-16 17:49:57 -0300

    Added functionality to redirect buttons on the View component
    Added the respective behaviors for the 'Edit this card' and the 'Make my own!' buttons
    in the View component. One keeps the parsed emoji card saved in the state while the
    other resets to the default state. Both will redirect to the Home component afterwards.

--- src/views/View.vue
+++ src/views/View.vue
@@ -3,18 +3,20 @@
     <Message :msg="state.message1" :editMode="false" />
     <EmojiWrapper :editMode="false" />
     <Message :msg="state.message2" :editMode="false" />
-    <button class="button-left">
-      Edit this card
-    </button>
-    <button class="button-right">
-      Make my own!
-    </button>
+    <div style="margin: 0.5em 0 2em 0;">
+      <button class="button-left" @click="saveAndRedirectToHome">
+        Edit this card
+      </button>
+      <button class="button-right" @click="setDefaultAndRedirectToHome">
+        Make my own!
+      </button>
+    </div>
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent, onMounted } from "vue";
-import { useRoute } from "vue-router";
+import { useRoute, useRouter } from "vue-router";
 
 import { QueryParamsObject, isQueryParamsObject } from "@/assets/types";
 
@@ -41,14 +43,12 @@ export default defineComponent({
     };
   },
   setup() {
+    const router = useRouter();
     const route = useRoute();
 
     onMounted(() => {
       if (state.allEmojis.size === 0) {
         loadEmojis();
-      }
-      if (state.activeEmojis.length === 0) {
-        setDefault();
       }
 
       console.log("Accessing route params...");
@@ -61,19 +61,55 @@ export default defineComponent({
       console.log(isQueryParamsObject(parameters));
 
       if (isQueryParamsObject(parameters)) {
-        console.log("Query parameters are OK.");
-        parseQueryParameters(parameters);
-        console.log(state.queryString);
+        try {
+          console.log("Query parameters are OK.");
+          // Load state from the query string:
+          parseQueryParameters(parameters);
+          // Recreate query string from loaded state:
+          generateQueryString();
+          console.log(state.queryString);
+        } catch (error) {
+          console.error(
+            "Failed parsing query string. Fallback is loading default state."
+          );
+          console.error(error);
+          setDefault();
+          generateQueryString();
+          console.log(state.queryString);
+        }
       } else {
         console.error(
-          "Query string didn't yield the correct Type. Fallback is loading current stored state."
+          "Query string didn't yield the correct Type. Fallback is loading default state."
         );
+        setDefault();
         generateQueryString();
         console.log(state.queryString);
       }
     });
 
-    return { onMounted };
+    function saveAndRedirectToHome() {
+      /*
+      Add the accessed card to the saved state when it is implemented.
+      Currently the card is already added to the staging state if the
+      query parameters are correctly parsed.
+      */
+      router.push({
+        name: "Home"
+      });
+    }
+
+    function setDefaultAndRedirectToHome() {
+      setDefault();
+      router.push({
+        name: "Home"
+      });
+    }
+
+    return {
+      onMounted,
+      saveAndRedirectToHome,
+      setDefaultAndRedirectToHome
+    };
   }
 });
 </script>
