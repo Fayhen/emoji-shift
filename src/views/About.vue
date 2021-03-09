daca585289f1129d0c1c7612d49878862632a444
Diego Souza <45882000+Fayhen@users.noreply.github.com>
2021-03-09 16:52:21 -0300

    Continuous Deployment to GitHub Pages (#14)
    Updated the CD workflow to push built application to a dedicated 'gh-pages' branch. Bumped version to '1.0.1'.

--- src/views/About.vue
+++ src/views/About.vue
@@ -4,7 +4,7 @@
     <h1>About Emoji-Shift:</h1>
     <p>Hi! Thank you for using my app! 😊</p>
     <p>
-      Emoji-Shift is a simple Web application made using <a href="https://v3.vuejs.org/" target="_blank" rel="noopener noreferrer">Vue.js 3</a> and <a href="https://www.typescriptlang.org/" target="_blank" rel="noopener noreferrer">TypeScript</a>. It was created as a learning experience, and a gift for a friend. 🎁
+      Emoji-Shift is a simple Web application made using <a href="https://v3.vuejs.org/" target="_blank" rel="noopener noreferrer">Vue.js 3</a> and <a href="https://www.typescriptlang.org/" target="_blank" rel="noopener noreferrer">TypeScript</a>. It was created as a learning experience, and a gift for someone special. 🎁
     </p>
     <p>
       I am continuing to make improvements to Emoji-Shift. If you would like to check its source code, it's available at <a href="https://github.com/Fayhen/emoji-shift" target="_blank" rel="noopener noreferrer">GitHub!</a>.
